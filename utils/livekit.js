bdc9118c2dec11164c96c4aeea363dca9e0856f0
Your Name <you@example.com>
2026-01-03 04:49:16 -0800

    Fix LiveKit token to return JWT string

--- utils/livekit.js
+++ utils/livekit.js
@@ -6,6 +6,11 @@ function createLiveKitToken({ room, userId, canPublish }) {
     process.env.LIVEKIT_API_SECRET,
     { identity: userId }
   );
+console.log(
+  'LIVEKIT ENV:',
+  !!process.env.LIVEKIT_API_KEY,
+  !!process.env.LIVEKIT_API_SECRET
+);
 
   at.addGrant({
     room,
